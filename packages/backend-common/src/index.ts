94917eee0997500894b89235fb8586469f86f169
rony <murony59@gmail.com>
2026-06-03 21:11:06 +0600

    changed env variables names

--- packages/backend-common/src/index.ts
+++ packages/backend-common/src/index.ts
@@ -1,3 +1,5 @@
-export const JWT_SECRET = process.env.JWT_SECRET || "123123"
-export const API_BASE_URL = process.env.API_BASE_URL || "http://localhost:3001" ;
-export const WS_BASE_URL = process.env.WS_BASE_URL || "ws://localhost:8080" ;
+export const JWT_SECRET = process.env.JWT_SECRET || "123123";
+
+export const API_BASE_URL =process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3001";
+
+export const WS_BASE_URL =process.env.NEXT_PUBLIC_WS_BASE_URL || "ws://localhost:8080";
